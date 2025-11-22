f638fe6a3b1906e1c850b6a108b5bbc6aa460b5c
Balashov2004 <sashabalashov2019@gmail.com>
2025-11-22 13:07:31 +0500

    add test

---
+++ ObjectPrinting/Tests/ObjectPrinterNestedTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace ObjectPrinting.Tests;
+
+[TestFixture]
+public class ObjectPrinterNestedTests
+{
+    [Test]
+    public void PersonWithNestedCompanyAndAddressTest()
+    {
+        var person = new Person
+        {
+            FirstName = "Ivanov",
+            Name = "Alex",
+            Age = 21,
+            Score = 99.9,
+            Id = Guid.NewGuid(),
+            Company = new Company
+            {
+                CompanyName = "Kontur",
+                Industry = "IT",
+                Address = new Address
+                {
+                    Street = "Maloprudnia 12",
+                    City = "EKB",
+                    Country = "Russia"
+                }
+            },
+            Address = new Address
+            {
+                Street = "88",
+                City = "Moscow",
+                Country = "Russia"
+            }
+        };
+
+        var printed = ObjectPrinter.For<Person>()
+            .Printing<double>(TypeNumber.WithDot)
+            .PrintToString(person);
+
+        Assert.That(printed, Contains.Substring("FirstName = Ivanov"));
+        Assert.That(printed, Contains.Substring("Name = Alex"));
+        Assert.That(printed, Contains.Substring("Age = 21"));
+        Assert.That(printed, Contains.Substring("Score = 99.9"));
+        Assert.That(printed, Contains.Substring("Id"));
+
+        Assert.That(printed, Contains.Substring("Company"));
+        Assert.That(printed, Contains.Substring("CompanyName = Kontur"));
+        Assert.That(printed, Contains.Substring("Industry = IT"));
+
+        Assert.That(printed, Contains.Substring("Street = Maloprudnia 12"));
+        Assert.That(printed, Contains.Substring("City = EKB"));
+        Assert.That(printed, Contains.Substring("Country = Russia"));
+
+        Assert.That(printed, Contains.Substring("Address"));
+        Assert.That(printed, Contains.Substring("Street = 88"));
+        Assert.That(printed, Contains.Substring("City = Moscow"));
+        Assert.That(printed, Contains.Substring("Country = Russia"));
+    }
+}
--- ObjectPrinting/Tests/Person.cs
+++ ObjectPrinting/Tests/Person.cs
@@ -1,14 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace ObjectPrinting.Tests
+namespace ObjectPrinting.Tests;
+
+public class Person
 {
-    public class Person
+    public string FirstName;
+    public Company Company;
+    public Address Address;
+    public List<string> Tags;
+    
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public double Score { get; set; }
+    public Guid Id { get; set; }
+
+    public Person()
     {
-        public string FirstName;
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public double Score { get; set; }
-        public Guid Id { get; set; }
-        
+        Tags = new List<string>();
     }
+}
+
+public class Company
+{
+    public string CompanyName { get; set; }
+    public string Industry { get; set; }
+    public Address Address { get; set; }
+}
+
+public class Address
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
 }
